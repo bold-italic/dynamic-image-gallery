95447e458fa661d104d4f0ebd54b5a839ad00203
bold-italic <msisdn.com@gmail.com>
2022-01-12 14:26:43 -0600

    Uploaded files

---
+++ README.md
@@ -0,0 +1,29 @@
+# Dynamic Image Gallery
+
+A dynamic image gallery using the Unsplash Image Source service.
+
+## Description
+
+This image gallery implemented in PHP allows you to view a random image from Unsplash from a specific category and locally saved images.
+
+## Demo
+
+Here is a working live demo: https://dynamic-image-gallery.epizy.com/
+
+## Built With
+
+* ![HTML5](https://img.shields.io/badge/-HTML5-E34F26.svg?style=flat&logo=html5&logoColor=white)
+* ![CSS3](https://img.shields.io/badge/-CSS3-1572B6.svg?style=flat&logo=css3&logoColor=white)
+* ![PHP](https://img.shields.io/badge/-PHP-777BB4.svg?style=flat&logo=php&logoColor=white)
+
+## Features
+
+* Mobile responsive design
+
+## Acknowledgments
+
+* [Unsplash](https://unsplash.com/developers)
+* [Sascha Bosshard](https://unsplash.com/@thesaboo)
+* [Shiqi Zhao](https://unsplash.com/@alger19)
+* [Diane Theresa Hendrick](https://unsplash.com/@artemisdesign)
+* [Simon Williams](https://unsplash.com/@simowilliams)
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+	$config = [
+			   'gallery_name' => 'Dynamic Image Gallery',
+			   'unsplash_categories' => ['Travel','Interiors','Film','Athletics'],
+			   'photographer'  => [
+			   		['local_images' => '01.jpg', 'name' => 'Sascha Bosshard', 'href' => 'https://unsplash.com/@thesaboo'],
+			   		['local_images' => '02.jpg', 'name' => 'Shiqi Zhao', 'href' => 'https://unsplash.com/@alger19'],
+			   		['local_images' => '03.jpg', 'name' => 'Diane Theresa Hendrick', 'href' => 'https://unsplash.com/@artemisdesign'],
+			   		['local_images' => '04.jpg', 'name' => 'Simon Williams', 'href' => 'https://unsplash.com/@simowilliams']
+			   ]
+ 			  ];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?= $config['gallery_name'] ?></title>
+	<link href="https://fonts.googleapis.com/css?family=Alegreya" rel="stylesheet">
+	<link rel="stylesheet" href="main.css">
+</head>
+<body>
+	<h1><?= $config['gallery_name'] ?></h1>
+	<div id="gallery">
+		<?php foreach($config['unsplash_categories'] as $categories): ?>
+        	<div>
+        		<h2><?= $categories ?></h2>
+        		<img src="https://source.unsplash.com/300x200/?<?= $categories ?>" alt="<?= $categories ?> image">
+        	</div>
+		<?php endforeach ?>
+	</div>
+	<h1><?= count($config['photographer']) ?> Large Images</h1>
+	<div id="large-images">
+		<?php foreach($config['photographer'] as $images): ?>
+        	<img src="images/<?= $images['local_images'] ?>" alt="<?= $images['name'] ?>">
+			<h3 class="photographer"><a href="<?= $images['href'] ?>"><?= $images['name'] ?></a></h3>
+		<?php endforeach ?>
+	</div>
+</body>
+</html>
